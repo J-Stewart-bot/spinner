95051e4b82f632108b68e3444c2847c1dc17a032
Justin Stewart <Justin.lighthouse.labs@gmail.com>
2019-09-24 20:38:42 +0000

    cleaned it up a bit

--- spinner2.js
+++ spinner2.js
@@ -1,5 +1,5 @@
 const spinners = ['|', '/', '-', '\\'];
-let delay = spinners.length * 10
+let delay = spinners.length * 10;
 for (let i = 0; i < delay; i++) {
   setTimeout(() => {
     process.stdout.write(`\r${spinners[i % spinners.length]}`);
